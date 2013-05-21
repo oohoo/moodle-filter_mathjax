03fefec0425a951c6697b32449deb18dfc1b392c
Raymond Wainman <raywainman@gmail.com>
2013-05-21 14:55:24 -0600

    updating version.php and adding dependencies in filter.php

--- filter.php
+++ filter.php
@@ -1,16 +1,45 @@
 <?php 
 
 /**
- * @author : Patrick Thibaudeau
- * @version $Id: version.php,v 1.0 2012/02/13
- * @package MathJax
- **/
-function mathjax_filter($courseid,$text) {
-	global $CFG;
+ * *************************************************************************
+ * *                               MathEditor                             **
+ * *************************************************************************
+ * @package     filter                                                    **
+ * @subpackage  mathjax                                                   **
+ * @name        MathJax                                                   **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Patrick Thibaudeau, Raymond Wainman                       **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
 
-        echo '<script type="text/javascript" src="'.$CFG->wwwroot.'/filter/mathjax/js/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>';
-	return $text;	
+class filter_mathjax extends moodle_text_filter {
+
+    /**
+     * Filters the text given and returns the result.
+     *
+     * @override
+     * @param $text some HTML content
+     * @param array $options options passed to the filter
+     * @return the HTML content after the filtering has been applied
+     */
+    public function filter($text, array $options = array()) {
+        // Nothing to be done as the MathJax code automatically detects the $$ delimiters
+        return $text;
+    }
+
+    /**
+     * Sets up the page before calling the filter, performs any setup tasks required.
+     *
+     * @override
+     * @param moodle_page $page the page we are going to add requirements to.
+     * @param context $context the context which contents are going to be filtered.
+     */
+    public function setup($page, $context) {
+        $page->requires->js(new moodle_url('/filter/mathjax/vendor/mathjax/MathJax.js', array(
+            'config' => 'TeX-AMS-MML_HTMLorMML',
+            'delayStartupUntil' => 'onload'
+        )));
+    }
 }
-
-
-?>
---
+++ version.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+ * *************************************************************************
+ * *                               MathEditor                             **
+ * *************************************************************************
+ * @package     filter                                                    **
+ * @subpackage  mathjax                                                   **
+ * @name        MathJax                                                   **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Patrick Thibaudeau, Raymond Wainman                       **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2013052100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires = 2013042600;         // Requires this Moodle version
+$plugin->component = 'filter_mathjax';  // Full name of the plugin (used for diagnostics)

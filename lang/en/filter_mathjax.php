c5fa194302a60650c6ea0d04c3affac7fd8bae6c
Raymond Wainman <raywainman@gmail.com>
2013-05-22 12:58:42 -0600

    removing unneeded settings page and adding header to language file

--- lang/en/filter_mathjax.php
+++ lang/en/filter_mathjax.php
@@ -1,7 +1,18 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+/**
+ * *************************************************************************
+ * *                               MathEditor                             **
+ * *************************************************************************
+ * @package     filter                                                    **
+ * @subpackage  mathjax                                                   **
+ * @name        MathJax                                                   **
+ * @copyright   oohoo.biz                                                 **
+ * @link        http://oohoo.biz                                          **
+ * @author      Patrick Thibaudeau, Raymond Wainman                       **
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
+ * *************************************************************************
+ * ************************************************************************ */
+
 $string['filtername'] = 'MathJax Filter';
-?>
+
